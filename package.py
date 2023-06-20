4b833eadf630a96e95a28d52f0c668c70d887694
Ron Woods <ron.woods@dreamworks.com>
2023-06-20 10:23:28 -0700

    Moonray 4900 (#108)
    * MOONRAY-4900: Added rocky-9 variants.
    
    * MOONRAY-4900: Added rocky-9 variants.
    
    * Bumped major version for the release.
    
    * MOONRAY-4900: Added rocky-9 variants.
    
    * MOONRAY-4952 Add CMake options to set CXX11 ABI and abi-version (#109)
    
    * Add CMake options to set CXX11 ABI and abi-version
    
    Signed-off-by: James Horsley <jbhorsley@gmail.com>
    
    * Don't define _GLIBCXX_USE_CXX11_ABI
    
    * Add option for -fabi-version
    
    * Don't print CMAKE_MODULE_PATH
    
    Signed-off-by: James Horsley <jbhorsley@gmail.com>
    Co-authored-by: James Horsley <jbhorsley@gmail.com>
    
    * Bumped minor version for the release.
    
    * MOONRAY-4900: Added rocky-9 variants.
    
    * MOONRAY-4900, MOONRAY-4980: Moved RaTS to refplat-vfx2021 and restrict CI tests to CentOS-7 variants.
    
    Signed-off-by: James Horsley <jbhorsley@gmail.com>
    Co-authored-by: tkato <toshi.kato@dreamworks.com>
    Co-authored-by: Jon Lanz <Jon.Lanz@dreamworks.com>
    Co-authored-by: James Horsley <jbhorsley@gmail.com>

--- package.py
+++ package.py
@@ -36,18 +36,23 @@ else:
     build_system_pbr = 'cmake_modules'
 
 variants = [
-    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2020.3', 'icc-19.0.5.281.x.2'],
-    ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2020.3', 'icc-19.0.5.281.x.2'],
-    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2020.3', 'gcc-6.3.x.2'],
-    ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2020.3', 'gcc-6.3.x.2'],
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'clang-13', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
+    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2020.3', 'icc-19.0.5.281.x.2'],
+    ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2020.3', 'icc-19.0.5.281.x.2'],
+    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2020.3', 'gcc-6.3.x.2'],
+    ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2020.3', 'gcc-6.3.x.2'],
+    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'opencolorio-2'],
+    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'opencolorio-2'],
+    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
+    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
 ]
 
 conf_rats_variants = variants[0:2]
+conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 scons_targets = ['@install'] + unittestflags
 sconsTargets = {
