f319274f6a726c6d0b0c06209b209def429b5e3c
jlanz <Jon.Lanz@dreamworks.com>
2023-12-12 16:07:36 -0800

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '15.7'
+    _version = '15.8'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -63,7 +63,7 @@ sconsTargets = {
 
 requires = [
     'mkl',
-    'moonray-15.7',
+    'moonray-15.8',
     'mcrt_denoise-4.4',
     'qt',
 ]
