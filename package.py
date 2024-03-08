99d238b74d2f6c6371c273ad912bb000efe3ea25
Ron Woods <ron.woods@dreamworks.com>
2024-03-08 07:54:28 -0800

    MOONRAY-5137, MOONRAY-5212: Remove scons build capability. (#118)

--- package.py
+++ package.py
@@ -2,10 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # -*- coding: utf-8 -*-
-import os, sys
-
-unittestflags = (['@run_all', '--unittest-xml']
-                 if os.environ.get('BROKEN_CUSTOM_ARGS_UNITTESTS') else [])
+import os
 
 name = 'moonray_gui'
 
@@ -31,17 +28,10 @@ authors = [
 help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
 
-if 'scons' in sys.argv:
-    build_system = 'scons'
-    build_system_pbr = 'bart_scons-10'
-else:
-    build_system = 'cmake'
-    build_system_pbr = 'cmake_modules'
-
 variants = [
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'opencolorio-2'],
-    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'clang-13', 'gcc-9.3.x.1', 'opencolorio-2'],
+    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'clang-13', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2020.3', 'icc-19.0.5.281.x.2'],
@@ -59,14 +49,6 @@ variants = [
 conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
-scons_targets = ['@install'] + unittestflags
-sconsTargets = {
-    'refplat-vfx2020.3': scons_targets,
-    'refplat-vfx2021.0': scons_targets,
-    'refplat-vfx2022.0': scons_targets,
-    'refplat-vfx2023.0': scons_targets,
-}
-
 requires = [
     'mkl',
     'moonray-15.16',
@@ -75,7 +57,7 @@ requires = [
 ]
 
 private_build_requires = [
-    build_system_pbr,
+    'cmake_modules',
     'cppunit',
 ]
 
@@ -86,14 +68,10 @@ testentry = lambda i: ("variant%d" % i,
 testlist = [testentry(i) for i in range(len(variants))]
 tests = dict(testlist)
 
-if build_system is 'cmake':
-    def commands():
-        prependenv('CMAKE_MODULE_PATH', '{root}/lib64/cmake')
-        prependenv('CMAKE_PREFIX_PATH', '{root}')
-        prependenv('PATH', '{root}/bin')
-else:
-    def commands():
-        prependenv('PATH', '{root}/bin')
+def commands():
+    prependenv('CMAKE_MODULE_PATH', '{root}/lib64/cmake')
+    prependenv('CMAKE_PREFIX_PATH', '{root}')
+    prependenv('PATH', '{root}/bin')
 
 uuid = '470184d5-82cf-4d9b-a94b-456f75bb97a1'
 
