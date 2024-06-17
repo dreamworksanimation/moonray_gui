b22bb76a81e3f8d5ee3fa87e770852ae7ffcfc0d
Ron Woods <ron.woods@dreamworks.com>
2024-06-17 16:49:06 -0700

    CM-18187: Added clang-17 variants for refplat-vfx2023. (#129)

--- package.py
+++ package.py
@@ -40,6 +40,8 @@ variants = [
     ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2022.0', 'gcc-9.3.x.1', 'opencolorio-2'],
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.0', 'gcc-11.x', 'opencolorio-2'],
     ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'gcc-11.x', 'opencolorio-2'],
+    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.0', 'clang-17.0.6.x', 'opencolorio-2'],
+    ['os-rocky-9', 'opt_level-debug', 'refplat-vfx2023.0', 'clang-17.0.6.x', 'opencolorio-2'],
 ]
 
 conf_rats_variants = variants[0:2]
