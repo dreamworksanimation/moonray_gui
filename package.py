a2f5f20c78bf49ef4a46703430c66b6b55fd3537
mrday <mike.day@dreamworks.com>
2024-10-01 15:42:00 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '16.24'
+    _version = '16.25'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -49,8 +49,8 @@ conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
     'mkl',
-    'moonray-16.24',
-    'mcrt_denoise-5.14',
+    'moonray-16.25',
+    'mcrt_denoise-5.15',
     'qt',
 ]
 
