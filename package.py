dab53ab31eb84e94d976b75c16721ee0cb0399eb
Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>
2025-04-04 12:42:00 -0700

    Fix variants (#143)

--- package.py
+++ package.py
@@ -32,7 +32,6 @@ variants = [
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'gcc-11.x'],
     ['os-rocky-9', 'opt_level-debug',    'refplat-vfx2023.1', 'gcc-11.x'],
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'clang-17.0.6.x'],
-    ['os-rocky-9', 'opt_level-debug',    'refplat-vfx2023.1', 'clang-17.0.6.x'],
     ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2022.0', 'gcc-9.3.x.1'],
 
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2022.0', 'gcc-9.3.x.1'],
